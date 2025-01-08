a90e1e9a93341375a0dd925fe90bf03eaafd7308
asma828 <asmalachhab66@gmail.com>
2025-01-08 10:05:11 +0100

    add article class

---
+++ classes/article.php
@@ -0,0 +1,51 @@
+<?php 
+class Article {
+    private $id_article;
+    private $title;
+    private $content;
+    private $image;
+    private $status;
+    private $id_theme;
+    private $id_user;
+    private $conn;
+
+    public function __construct($conn,$title="",$content="",$image="",$status="",$id_theme=0,$id_user=0,){
+    $this->conn=$conn; 
+    $this->title=$title;
+    $this->content=$content;
+    $this->image=$image;
+    $this->status=$status;
+    $this->id_theme=$id_theme;
+    $this->id_user=$id_user; 
+    
+    }
+public function getId(){
+    return $this->id_article;
+}
+public function getTitle(){
+    return $this->title;
+}
+
+public function getContent(){
+    return $this->content;
+}
+
+public function getImage(){
+    return $this->image;
+}
+
+public function getStatus(){
+    return $this->status;
+}
+
+public function getThemeId(){
+    return $this->id_theme;
+}
+
+public function getUserId(){
+    return $this->id_user;
+}
+
+
+}
+?>
